8bc3d472930d3c441928d42187faf4e12f8b2c09
Subham <sd.jpg996@gmail.com>
2023-07-02 13:35:02 +0530

    Singleton

---
+++ Client.java
@@ -0,0 +1,10 @@
+public class Client {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+        Singleton s4 = Singleton.getInstance();
+
+
+    }
+}
